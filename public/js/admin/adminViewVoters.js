d4533e968c93f2f3e1bcee79dd307c8b0d9b02cf
KishanSola104 <kishan104.solanki@gmail.com>
2025-07-25 15:03:14 +0530

    add Functionality for Delete and View Voters for admin Dashboard

---
+++ public/js/admin/adminViewVoters.js
@@ -0,0 +1,49 @@
+window.initViewVoters=function(){
+
+    console.log("Admin View Voters JS Loaded");
+
+    
+  // get the selected state and district from the session storage
+  const selectedStateDistrict = JSON.parse(
+    sessionStorage.getItem("selectedStateDistrict")
+  );
+
+  if (
+    !selectedStateDistrict ||
+    !selectedStateDistrict.state ||
+    !selectedStateDistrict.district
+  ) {
+    alert("No state or district selected");
+    return;
+  }
+
+  const stateDropdown = document.getElementById("voterState");
+  const districtDropdown = document.getElementById("voterDistrict");
+  if (!stateDropdown || !districtDropdown) {
+    alert("State or District dropdown not found in the DOM.");
+    return;
+  }
+  stateDropdown.value = selectedStateDistrict.state;
+  districtDropdown.value = selectedStateDistrict.district;
+
+  // Search logic
+  const voterIdInput = document.getElementById("searchVoterId");
+  const searchButton = document.getElementById("searchVoterBtn");
+
+  function handleSearch() {
+    const voterID = voterIdInput.value.trim();
+    if (!voterID) {
+      alert("Please Enter a Valid Voter ID");
+      return;
+    }
+    // fetchCandidateData(voterID);
+  }
+
+  searchButton.addEventListener("click", handleSearch);
+  voterIdInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  });
+
+};
